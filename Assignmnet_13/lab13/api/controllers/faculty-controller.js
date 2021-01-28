126ca5f6302e3e8c67bbc9439e8a826b2b75d5d3
wegahta <gehawegahta@gmail.com>
2021-01-27 22:21:54 -0600

    adding

---
+++ Assignmnet_13/lab13/api/controllers/faculty-controller.js
@@ -0,0 +1,16 @@
+// const path=require("path");
+//  const dispatchPage=function(pageName, res){
+//     const response={
+//         status:200,
+//         message: "Index page"
+//        }
+//     res.status(response.status).sendFile(path.join(__dirname, "..","..","public","fucalty_pages",pageName+".html"));
+//  }
+//    module.exports.facultyIndex=function(req, res){
+//     console.log("are you  reached here")
+
+//     dispatchPage("faculty",res);
+    
+//     }
+
+    
